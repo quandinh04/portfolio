face8b6455cb0fafc634a9facd866c800ccbbb02
Đinh Thế Quân <thequan2004@gmail.com>
2021-09-16 01:12:15 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
-
           {/* FAVICONS */}
           <link rel="icon" href="/catIcon.png" />
           <meta name="theme-color" content="#ffffff"></meta>
+          {/* GOOGLE SEO */}
+          <meta name="google-site-verification" content="cyKrx6Pn6xYH8mG2dqWRxWnO1DGvGr5uUWaImj0tJAU" />
         </Head>
         <body className="transition-colors duration-200 bg-white dark:bg-light-black">
           <Main />
