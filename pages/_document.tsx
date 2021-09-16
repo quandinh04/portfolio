8d96dad688b29de2b1982c4f0e851cec374ff213
Đinh Thế Quân <thequan2004@gmail.com>
2021-09-16 08:37:21 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="google-site-verification" content="cyKrx6Pn6xYH8mG2dqWRxWnO1DGvGr5uUWaImj0tJAU" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
